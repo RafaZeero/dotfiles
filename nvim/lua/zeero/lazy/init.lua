421a19161b2c95f46562ca2b97b97f3e57bb89d6
RafaZeero <rafaa99@gmail.com>
2024-09-12 08:58:35 -0300

    add LSPs & cleanup init file

--- nvim/lua/zeero/lazy/init.lua
+++ nvim/lua/zeero/lazy/init.lua
@@ -408,6 +408,14 @@ return {
 					},
 					tailwindcss = {},
 					eslint = {},
+					html = { filetypes = { "html", "hbs" } },
+					cssls = {},
+					dockerls = {},
+					sqlls = {},
+					jsonls = {},
+					yamlls = {},
+					nginx_language_server = {},
+
 					-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 					--
 					-- Some languages (like typescript) have entire language plugins that can be useful:
