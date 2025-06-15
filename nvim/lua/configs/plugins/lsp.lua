6edf5f551e550bdfaafe6dcb6f4356633d8f09de
RafaZeero <rafaa99@gmail.com>
2025-06-15 16:01:12 -0300

    update lsp

--- nvim/lua/configs/plugins/lsp.lua
+++ nvim/lua/configs/plugins/lsp.lua
@@ -87,7 +87,7 @@ return {
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+          -- map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
@@ -179,8 +179,11 @@ return {
         },
         pyright = {
           settings = {
+            pyright = {
+              disableOrganizeImports = true,
+              disableTaggedHints = true,
+            },
             python = {
-              disableOrganizeImports = false,
               analysis = {
                 typeCheckingMode = "standard", -- Opções: off, basic, strict, standard
                 diagnosticMode = "openFilesOnly", -- Ou "workspace" para analisar todos os arquivos
@@ -196,7 +199,7 @@ return {
                 },
                 supportDocstringTemplate = true,
                 typeEvaluation = {
-                  enableReachabilityAnalysis = true,
+                  enableReachabilityAnalysis = false,
                   strictDictionaryInference = true,
                   strictListInference = true,
                   strictSetInference = true,
@@ -210,9 +213,16 @@ return {
           init_options = {
             settings = {
               organizeImports = false,
-              fixAll = true,
+              fixAll = false,
               -- Any extra CLI arguments for `ruff` go here.
               args = {},
+              lint = {
+                isort = {
+                  -- VERIFICAR ONDE FICA SAPORRA CARAIO!!
+                  -- combineAsImports = true, -- Habilita a combinação de imports
+                  -- forceWrapAliases = true, --
+                },
+              },
             },
           },
           on_attach = function(client)
