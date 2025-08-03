f5fa702684e40fd0b2d9dedd3e33591c280fd3b7
RafaZeero <rafaa99@gmail.com>
2025-08-02 23:22:02 -0300

    feat: update autocmds

--- nvim/lua/configs/autocmds.lua
+++ nvim/lua/configs/autocmds.lua
@@ -5,11 +5,11 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- local client = vim.lsp.start_client({
@@ -28,3 +28,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- 		vim.lsp.buf_attach_client(0, client)
 -- 	end,
 -- })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    vim.opt_local.set.number = false
+    vim.opt_local.set.relativenumber = false
+    vim.opt_local.set.scrolloff = 0
+  end,
+})
